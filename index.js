1153b3c70be27016da3d0ce395f8ce8b63f3640b
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2021-10-10 18:11:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const base64Pkg = require("base-64");
+const utf8 = require("utf8");
+
+const base64 = {
+  encode: (string) => {
+    const bytes = utf8.encode(string);
+
+    return base64Pkg.encode(bytes);
+  },
+  decode: (string) => {
+    const bytes = base64Pkg.encode(string);
+
+    return utf8.decode(bytes);
+  },
+};
+
+module.exports = base64;
